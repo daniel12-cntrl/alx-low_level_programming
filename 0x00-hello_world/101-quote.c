7f618bf7fd825f06768b6a11e6fedd0b9cf3971a
daniel12-cntrl <daniopia6@gmail.com>
2022-03-10 14:28:33 -0800

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,23 @@
-#include <unistd.h>
-
+#include <stdio.h>
 /**
- * main - Prints quote to the standard error
+ * main - outputs to string
  *
- * Return: zero on success
+ * Description: Prints out "and that piece of art is useful..." without printf
  *
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-	return (1);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
+return (1);
 }
